019e1041999f935d140e0eb9a409421efab0d391
bordy20 <bordy20@yahoo.com>
2023-09-01 10:13:04 +0800

    Update providers.tf
    removed entries

--- providers.tf
+++ providers.tf
@@ -16,9 +16,7 @@ terraform {
 }
 
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAWJKRSTHN6AWIJENC"
-  secret_key = "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"
+  region     = "default"
 }
 
 provider "azurerm" {
